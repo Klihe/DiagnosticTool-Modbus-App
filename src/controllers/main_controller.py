5afe8415f3020ef7df34fe74c8ce594bd179e9b5
Klihe <stepan.birke@gmail.com>
2024-07-29 12:17:24 +0200

    feat TableController prepared

--- src/controllers/main_controller.py
+++ src/controllers/main_controller.py
@@ -5,6 +5,7 @@ from ui.main_window import MainWindow
 from controllers.menu_controller import MenuController
 from controllers.tool_controller import ToolController
 from controllers.comm_controller import CommController
+from controllers.table_controller import TableController
 
 from models.ports import find_ports
 
@@ -16,6 +17,7 @@ class MainController:
         self.__menu_controller = MenuController(self.__main_window)
         self.__tool_controller = ToolController(self.__main_window)
         self.__comm_controller = CommController(self.__main_window)
+        self.__table_controller = TableController(self.__main_window)
     
     def show(self) -> None:
         self.__main_window.show()
---
+++ src/controllers/table_controller.py
@@ -0,0 +1,14 @@
+from PyQt6.QtWidgets import QMainWindow
+
+class TableController:
+    def __init__(self, parent: QMainWindow) -> None:
+        self.__parent = parent
+
+        self.__find()
+        self.__connect()
+        
+    def __find(self) -> None:
+        pass
+        
+    def __connect(self) -> None:
+        pass
